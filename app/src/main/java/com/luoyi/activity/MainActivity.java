55c507de7b964994febae359eb9c44612c6dd94d
wuwenchao <973892232@qq.com>
2017-03-18 23:36:00 +0800

    Initial commit

---
+++ app/src/main/java/com/luoyi/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luoyi.activity;
+import android.os.Bundle;
+import com.luoyi.luoyiims.R;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.x;
+import com.luoyi.activity.base.BaseActivity;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
