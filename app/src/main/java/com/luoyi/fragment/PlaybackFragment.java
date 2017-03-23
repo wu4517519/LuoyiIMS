8a8677d97c010a5e982f15b8489a477b0edac33c
wuwenchao <973892232@qq.com>
2017-03-23 23:40:43 +0800

    底部导航栏

---
+++ app/src/main/java/com/luoyi/fragment/PlaybackFragment.java
@@ -0,0 +1,56 @@
+package com.luoyi.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.luoyi.fragment.base.BaseFragment;
+import com.luoyi.luoyiims.R;
+
+public class PlaybackFragment extends BaseFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PlaybackFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment MonitorFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlaybackFragment newInstance(String param1) {
+        PlaybackFragment fragment = new PlaybackFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_playback, container, false);
+    }
+
+
+}
