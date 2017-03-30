6d1fc2800666cc119872b321c71fc97fb2c8d48b
CSIR <605337512@qq.com>
2017-03-30 13:47:40 +0800

    .

--- app/src/main/java/com/luoyi/activity/MainActivity.java
+++ app/src/main/java/com/luoyi/activity/MainActivity.java
@@ -1,15 +1,20 @@
 package com.luoyi.activity;
-import android.os.Bundle;
-import com.luoyi.luoyiims.R;
-import org.xutils.view.annotation.ContentView;
-import org.xutils.x;
-import com.luoyi.activity.base.BaseActivity;
 
-@ContentView(R.layout.activity_main)
+import android.os.Bundle;
+
+import com.luoyi.activity.base.BaseActivity;
+import com.luoyi.luoyiims.R;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_my)
 public class MainActivity extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
