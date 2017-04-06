7ce03c5d1482b97ce59a00476dbdc3f4dd78389c
CSIR <605337512@qq.com>
2017-04-06 11:16:49 +0800

    .

--- app/src/main/java/com/luoyi/fragment/LogFragment.java
+++ app/src/main/java/com/luoyi/fragment/LogFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.luoyi.fragment.base.BaseFragment;
 import com.luoyi.luoyiims.R;
@@ -11,7 +13,7 @@ import com.luoyi.luoyiims.R;
 public class LogFragment extends BaseFragment {
 
     private static final String ARG_PARAM1 = "param1";
-
+    private ListView log_lv;
     public LogFragment() {
         // Required empty public constructor
     }
@@ -28,8 +30,62 @@ public class LogFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false);
+
+        View view =inflater.inflate(R.layout.fragment_log, container, false);
+        log_lv = (ListView)view.findViewById(R.id.log_lv);
+        refreshView();
+        return view;
     }
 
+    private MyAdapter myadapter;
+    private void refreshView() {
+        if(myadapter==null){
+
+            myadapter = new MyAdapter();
+            log_lv.setAdapter(myadapter);
+        }else{
+
+            myadapter.notifyDataSetChanged();
+        }
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v ;
+            if(convertView==null){
+                v=View.inflate(getActivity(), R.layout.item_log, null);
+            }else{
+                v = convertView;
+            }
+
+
+            return v;
+        }
+
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+
+
+    }
 
 }
