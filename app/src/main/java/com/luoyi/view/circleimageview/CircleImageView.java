e442b1ada71770171e2a5a94824c73eca8aa5922
wu4517519 <973892232@qq.com>
2017-05-06 21:17:04 +0800

    调整文件位置

--- app/src/main/java/com/luoyi/view/circleimageview/CircleImageView.java
+++ app/src/main/java/com/luoyi/view/circleimageview/CircleImageView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.luoyi.utils.circleimageview;
+package com.luoyi.view.circleimageview;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
