e194ca44c286403dd875ba17aa8439a17e41b24e
wwc <973892232@qq.com>
2017-10-08 17:18:58 +0800

    fix

--- app/src/main/java/com/luoyi/fragment/MonitorLogFragment.java
+++ app/src/main/java/com/luoyi/fragment/MonitorLogFragment.java
@@ -1,6 +1,5 @@
 package com.luoyi.fragment;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,56 +12,24 @@ import android.widget.ListView;
 import com.luoyi.fragment.base.BaseFragment;
 import com.luoyi.luoyiims.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MonitorLogFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MonitorLogFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MonitorLogFragment extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private MyAdapter myadapter;
     private OnFragmentInteractionListener mListener;
     private ListView monitor_log_lv;
 
     public MonitorLogFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MonitorLogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MonitorLogFragment newInstance(String param1, String param2) {
+    public static MonitorLogFragment newInstance() {
         MonitorLogFragment fragment = new MonitorLogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -77,10 +44,8 @@ public class MonitorLogFragment extends BaseFragment {
         return view;
     }
 
-    private MyAdapter myadapter;
+
     private void refreshView() {
-
-
         myadapter = new MyAdapter();
         monitor_log_lv.setAdapter(myadapter);
 
@@ -88,12 +53,10 @@ public class MonitorLogFragment extends BaseFragment {
 
     private class MyAdapter extends BaseAdapter {
 
-
         @Override
         public int getCount() {
             return 3;
         }
-
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
